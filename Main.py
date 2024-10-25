87c4c426168bbda6349933bf09cfd6887b37ef96
IgSka <gaydukovgo@gmail.com>
2024-10-25 16:35:49 +0300

    Добавлен интерфейс: поле ввода, кнопка и метка. Добавлена функция show_coordinates

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
 from opencage.geocoder import OpenCageGeocode
+from tkinter import *
+
 
 
 
@@ -17,11 +19,28 @@ def get_coordinates(city,key):
 
 
 
+def show_coordinates():
+    city = entry.get()
+    coordinates = get_coordinates(city, key)
+    label.config(text=f"Координаты города {city}: {coordinates}")
+
 
 key = '34e0decbd98f44818554def72d211981'
-city = "Эквадор"
-coordinates = get_coordinates(city,key)
-print(f"Координаты города {city}: {coordinates}")
 
 
+
+window = Tk()
+window.title("Координаты городов")
+window.geometry('200x100')
+
+entry = Entry()
+entry.pack()
+
+button = Button(text="Поиск координат", command=show_coordinates)
+button.pack()
+
+label = Label(text="Введите город и нажмите на кнопку")
+label.pack()
+
+window.mainloop()
 
