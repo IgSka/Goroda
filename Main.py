fe5053acc87bec18a0c8251f84f42eb73b9bc786
IgSka <gaydukovgo@gmail.com>
2024-10-25 16:43:46 +0300

    Добавлен ввод с помощью клавиши Enter на клавиатуре

--- Main.py
+++ Main.py
@@ -19,10 +19,10 @@ def get_coordinates(city,key):
 
 
 
-def show_coordinates():
+def show_coordinates(event=None):
     city = entry.get()
     coordinates = get_coordinates(city, key)
-    label.config(text=f"Координаты города {city}: {coordinates}")
+    label.config(text=f"Координаты города {city}:\n {coordinates}")
 
 
 key = '34e0decbd98f44818554def72d211981'
@@ -31,10 +31,12 @@ key = '34e0decbd98f44818554def72d211981'
 
 window = Tk()
 window.title("Координаты городов")
-window.geometry('200x100')
+window.geometry('320x100')
 
 entry = Entry()
 entry.pack()
+entry.bind("<Return>", show_coordinates)
+
 
 button = Button(text="Поиск координат", command=show_coordinates)
 button.pack()
